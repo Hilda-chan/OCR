b7a45d8fb28ba7dc37f63c9ed9523a9f299ca2ce
hilda <kineval77@gmail.com>
2021-10-07 23:24:18 +0200

    reorganisation done

--- img/reorganise/blackxwhite.h
+++ img/reorganise/blackxwhite.h
@@ -1,6 +1,6 @@
 #ifndef BLACKXWHITE_H_
 #define BLACKXWHITE_H_
 
-SDL_Surface* blackwhite(SDL_Surface *img);
+SDL_Surface* blackwhite(SDL_Surface *image_surface,SDL_Surface *screen_surface);
 
 #endif
--- img/reorganise/main.c
+++ img/reorganise/main.c
@@ -6,88 +6,27 @@
 #include <stdlib.h>
 #include "blackxwhite.h"
 #include "or_logic.h"
-
-/*void init_sdl()
-{
-    // Init only the video part.
-    // If it fails, die with an error message.
-    if(SDL_Init(SDL_INIT_VIDEO) == -1)
-        errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
-}
-
-
-SDL_Surface* load_image(char *path)
-{
-    SDL_Surface *img;
-
-    // Load an image using SDL_image with format detection.
-    // If it fails, die with an error message.
-    img = IMG_Load(path);
-    if (!img)
-        errx(3, "can't load %s: %s", path, IMG_GetError());
-
-    return img;
-}
-
-
-SDL_Surface* display_image(SDL_Surface *img)
-{
-    SDL_Surface *screen;
-
-    // Set the window to the same size as the image
-    screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
-    if (screen == NULL)
-    {
-        // error management
-        errx(1, "Couldn't set %dx%d video mode: %s\n",
-                img->w, img->h, SDL_GetError());
-    }
-
-    // Blit onto the screen surface
-    if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
-        warnx("BlitSurface error: %s\n", SDL_GetError());
-
-    // Update the screen
-    SDL_UpdateRect(screen, 0, 0, img->w, img->h);
-
-    // return the screen for further uses
-    return screen;
-}
-
-
-void wait_for_keypressed()
-{
-    SDL_Event event;
-
-    // Wait for a key to be down.
-    do
-    {
-        SDL_PollEvent(&event);
-    } while(event.type != SDL_KEYDOWN);
-
-    // Wait for a key to be up.
-    do
-    {
-        SDL_PollEvent(&event);
-    } while(event.type != SDL_KEYUP);
-}
-
-
-void SDL_FreeSurface(SDL_Surface *surface);
-
-*/
-
+#include "display.h"
+    
 int main(int argc,char *argv[])
 {
     if(argc!=2)
         return EXIT_FAILURE;
-   SDL_Surface *img = load_image(argv[1]);
-    blackwhite(img);
+    
+    
+    SDL_Surface* image_surface = load_image(argv[1]);
+    SDL_Surface* screen_surface= display_image(image_surface);
+    SDL_Surface* image_blackxwhite;
+    SDL_Surface* image_filtered;    
+
+
+    image_blackxwhite = blackwhite(image_surface,screen_surface);
+    image_filtered = ET(image_blackxwhite,) 
+
     
 
     return 0;
 }
-
 
 
 
